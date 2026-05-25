f02101fca246d186095c74f722c15d5290072e71
werflub <denflibustier@gmail.com>
2026-05-25 11:55:21 +0400

    Update

--- app/src/main/java/com/example/util/CurrencyFetcher.kt
+++ app/src/main/java/com/example/util/CurrencyFetcher.kt
@@ -21,8 +21,8 @@ object CurrencyFetcher {
         }
         
         try {
-            val urlStr = "https://open.er-api.com/v6/latest/\$from"
-            Log.d("CurrencyFetcher", "Fetching: \$urlStr")
+            val urlStr = "https://open.er-api.com/v6/latest/$from"
+            Log.d("CurrencyFetcher", "Fetching: $urlStr")
             val url = URL(urlStr)
             val connection = url.openConnection() as HttpURLConnection
             connection.requestMethod = "GET"
@@ -30,7 +30,7 @@ object CurrencyFetcher {
             connection.readTimeout = 10000
 
             val responseCode = connection.responseCode
-            Log.d("CurrencyFetcher", "Response code: \$responseCode")
+            Log.d("CurrencyFetcher", "Response code: $responseCode")
             
             if (responseCode == 200) {
                 val jsonStr = connection.inputStream.bufferedReader().readText()
@@ -39,21 +39,21 @@ object CurrencyFetcher {
                     val rates = json.getJSONObject("rates")
                     if (rates.has(to)) {
                         val rate = rates.getDouble(to)
-                        Log.d("CurrencyFetcher", "Found rate for \$pair: \$rate")
+                        Log.d("CurrencyFetcher", "Found rate for $pair: $rate")
                         ratesCache[pair] = rate
                         lastFetchTime = now
                         return@withContext rate
                     } else {
-                        Log.e("CurrencyFetcher", "Rate for \$to not found in response")
+                        Log.e("CurrencyFetcher", "Rate for $to not found in response")
                     }
                 } else {
                     Log.e("CurrencyFetcher", "API error: " + json.optString("error-type"))
                 }
             } else {
-                Log.e("CurrencyFetcher", "Error response: \$responseCode")
+                Log.e("CurrencyFetcher", "Error response: $responseCode")
             }
         } catch (e: Exception) {
-            Log.e("CurrencyFetcher", "Exception: \${e.message}", e)
+            Log.e("CurrencyFetcher", "Exception: ${e.message}", e)
         }
         null
     }
